f587b981f2a4b7a641eb981c046e97147deb67cf
lie94 <felix94h@gmail.com>
2015-10-02 15:06:57 +0200

    L1 uppg 1,2

---
+++ PROLOG/L1.pl
@@ -0,0 +1,26 @@
+fib(0,0).
+fib(1,1).
+
+fib(N, F):-
+	N > 0,
+	K is (N - 1),
+	fibHelp(K, F, 0, 1).
+
+fibHelp(0, F, _, F).
+
+fibHelp(N, F, PREVIOUSLY, VALUE):-
+	N1 is (N - 1),
+	VALUE1 is (PREVIOUSLY + VALUE),
+	fibHelp(N1, F, VALUE, VALUE1).
+
+rovarsprak([],[]).
+rovarsprak([H|T1],[H,111,H|T2]):-
+	not(member(H,[97,101,105,111,117,121])),
+	rovarsprak(T1,T2).	
+rovarsprak([H|T1],[H|T2]):-
+	rovarsprak(T1,T2).
+
+medellangd(Text, AvgLen) :- AvgLen = 1.0.
+skyffla(Lista, Skyfflad) :- Skyfflad = Lista.
+
+main.
