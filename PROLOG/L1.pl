eba5a9d43473b9e58d13baa4b92cb41b916dae33
lie94 <felix94h@gmail.com>
2015-10-04 16:01:04 +0200

    Fixade kattiskontrollerna över de två gjorda uppgifterna.

--- PROLOG/L1.pl
+++ PROLOG/L1.pl
@@ -1,12 +1,12 @@
-fib(0,0).
-fib(1,1).
+fib(0,0):- !.
+fib(1,1):- !.
 
 fib(N, F):-
 	N > 0,
 	K is (N - 1),
 	fibHelp(K, F, 0, 1).
 
-fibHelp(0, F, _, F).
+fibHelp(0, F, _, F):- !.
 
 fibHelp(N, F, PREVIOUSLY, VALUE):-
 	N1 is (N - 1),
@@ -16,11 +16,11 @@ fibHelp(N, F, PREVIOUSLY, VALUE):-
 rovarsprak([],[]).
 rovarsprak([H|T1],[H,111,H|T2]):-
 	not(member(H,[97,101,105,111,117,121])),
-	rovarsprak(T1,T2).	
+	rovarsprak(T1,T2),
+	!.	
 rovarsprak([H|T1],[H|T2]):-
 	rovarsprak(T1,T2).
 
 medellangd(Text, AvgLen) :- AvgLen = 1.0.
 skyffla(Lista, Skyfflad) :- Skyfflad = Lista.
-
-main.
+% Man kan inte ha en mainfunktion när man lämnar in på kattis då den inte fungerar i det fallet.
