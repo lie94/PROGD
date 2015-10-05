6dc37709e7e0f79841557d1856d6271c6945cee6
lie94 <felix94h@gmail.com>
2015-10-05 11:14:29 +0200

    Done with lab L1

--- PROLOG/L1.pl
+++ PROLOG/L1.pl
@@ -14,36 +14,60 @@ fibHelp(N, F, PREVIOUSLY, VALUE):-
 	VALUE1 is (PREVIOUSLY + VALUE),
 	fibHelp(N1, F, VALUE, VALUE1).
 
-rovarsprak([],[]).
+rovarsprak([],[]).	
 rovarsprak([H|T1],[H,111,H|T2]):-
 	not(member(H,[97,101,105,111,117,121])),
-	rovarsprak(T1,T2),
-	!.	
+	!,
+	rovarsprak(T1,T2).
 rovarsprak([H|T1],[H|T2]):-
 	rovarsprak(T1,T2).
 
-medellangd(Text, AvgLen):- helpLangd(Text, AvgLen,0, 0, 0).
+medellangd([],0):- !.
+medellangd(Text, AvgLen):- 
+	!,
+	helpLangd(Text,32, AvgLen, 0, 0).	
 
 helpLangd([H|Tail]	, LastChar, AvgLen, Total, Gaps):-
 	notLetter(H),
 	notLetter(LastChar),
+	!,
 	helpLangd(Tail,H,AvgLen, Total, Gaps).
 
 helpLangd([H|Tail]	, 		_, AvgLen, Total, Gaps):-
 	notLetter(H),
+	!,
 	helpLangd(Tail, H, AvgLen, Total, (Gaps + 1)).
-	%helpLangd(Tail, 		H, AvgLen, Total, (Gaps + 1)).
 
 helpLangd([H|Tail]			, _, AvgLen, Total, Gaps):-
+	!,
 	helpLangd(Tail, H, AvgLen, (Total + 1), Gaps).
 
-helpLangd([]				, _, AvgLen, Total, Gaps):- 
+helpLangd([]				, LastChar, AvgLen, Total, Gaps):- 
+	notLetter(LastChar),
 	!,
-	AvgLen = Total / Gaps.
+	AvgLen is Total / Gaps.
+
+helpLangd([]				, _, AvgLen, Total, Gaps):- 
+	AvgLen is Total / (Gaps + 1).
 
 notLetter(Char):-
-	member(Char,[32,33,46,39,38]).
+	not(is_alpha(Char)). %member(Char,[32,33,46,39,38]).
 
-skyffla(Lista, Skyfflad) :- Skyfflad = Lista.
-main.
+%skyffla(Lista, Skyfflad) :- skyfflahelp()	
+skyffla(Lista,Skyffla):-
+	everyother(Lista,X),
+	removeFirst(Lista,NewList),
+	everyother(NewList,L),
+	!,
+	skyffla(L,Y),
+	append(X,Y,Skyffla).
+skyffla([],Skyffla):- Skyffla = [].
+
+removeFirst([_|Tail],L):- L = Tail. 
+
+everyother([H1,_|Tail],OutList):-
+	everyother(Tail,L),
+	append([H1],L,OutList).
+everyother([],[]).
+everyother(OneAtom, OutList):- OneAtom = OutList.
 % Man kan inte ha en mainfunktion när man lämnar in på kattis då den inte fungerar i det fallet.
