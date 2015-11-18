3596940c709b4aef196b1298686d6f24b136bdf0
lie94 <felix94h@gmail.com>
2015-11-18 16:19:24 +0100

    Parser3

--- leoparser3.py
+++ leoparser3.py
@@ -57,27 +57,26 @@ REP 			= "^\s*REP\s*$"
 QUOTE 			= "^\s*\"\s*$" 
 WHITESPACE 		= "^\s+$"
 NUMBER 			= "^\s*\d+\s*$"
-HASH 			= "\s*^#\s*$"  
+HASH 			= "\s*^#\d{6}\s*$"  
 PERIOD 			= "^\s*\.\s*$"
 ALLOPTIONS 		= [FORW,BACK,LEFT,RIGHT,DOWN,UP,COLOR,REP,QUOTE,WHITESPACE,NUMBER,HASH,PERIOD]
 #ANYVALID = WHITESPACE + "|" + NUMBER + "|" + HASHNUMBER + "|" + FORW + "|" + BACK + "|" + LEFT + "|" + RIGHT + "|" + DOWN + "|" + UP + "|" + COLOR + "|" + REPBEGIN + "|" + QUOTE + "|" + PERIOD
 
-
-
 def main():
 	pre_lex 	= stdin.read().upper()
+	pre_lex		= re.sub("\t",'',pre_lex)
+	#print pre_lex
 	post_lex 	= lexer(pre_lex)
-	if 14 in post_lex:
-		error(pre_lex,post_lex.index(14))
-		return
-	post_parse 	= parse(post_lex,[],1)
+	print "post_text: " + str(post_lex)
+	post_parse 	= parse(post_lex,[],1,0)
 	if not(isinstance(post_parse,list)):
 		error(pre_lex,post_parse)
 		return
-	print post_lex
+	print "post_parse:" + str(post_parse)
 def lexer(s_input):
 	s_input = re.sub("(\%.*)?\n",' ', s_input)
-	temp = [i for i in re.split(r'(\s+|\.|\d+|\W+)', s_input) if i]
+	temp = [i for i in re.split(r'(\#\d+|\s+|\.|\d+|\W+)', s_input) if i]
+	print temp
 	tokens = []
 	for token in temp:
 		valid = False
@@ -92,26 +91,56 @@ def lexer(s_input):
 		if not(valid):
 			tokens.append(14)
 	return tokens
-def parse(l_input,syntaxtree,counter):
-	if counter = len(l_input):
+def parse(l_input,syntaxtree,counter,repCounter):
+	print "l_input[" + str(counter - 1) + "]: " + str(l_input[counter - 1])
+	if counter == len(l_input):
 		return syntaxtree
-	elif l_input[counter - 1] in [FORW_N, BACK_N, LEFT_N, RIGHT_N]:
+	elif l_input[counter - 1] in [FORW_N, BACK_N, LEFT_N, RIGHT_N]: 							#RIGHT LEFT BACK FORW
 		if l_input[counter] == WHITESPACE_N and isinstance(l_input[counter + 1],list):
 			if l_input[counter + 2] == WHITESPACE_N and l_input[counter + 3] == PERIOD_N:
 				syntaxtree.append([l_input[counter - 1],l_input[counter + 1][1]])
-				return parse(l_input,syntaxtree,counter + 3)
+				return parse(l_input,syntaxtree,counter + 5, repCounter)
 			elif l_input[counter + 2] == PERIOD_N:
 				syntaxtree.append([l_input[counter - 1],l_input[counter + 1][1]])
-				return parse(l_input,syntaxtree,counter + 2)
-	elif l_input[counter - 1] in [UP_N, DOWN_N]:
-					
-	elif l_input[counter - 1] in WHITESPACE_N:
-		return parse(l_input,syntaxtree,counter + 1)
+				return parse(l_input,syntaxtree,counter + 4, repCounter)
+
+
+	elif l_input[counter - 1] in [UP_N, DOWN_N]:												#DOWN UP
+		if l_input[counter] == WHITESPACE_N and l_input[counter + 1] == PERIOD_N:
+			syntaxtree.append([l_input[counter - 1], 0]) # 0 Eftersom vi inte har någon styrsiffra som i de andra fallen
+			return parse(l_input,syntaxtree,counter + 3, repCounter)
+		elif l_input[counter] == PERIOD_N:
+			syntaxtree.append([l_input[counter - 1], 0])
+			return parse(l_input,syntaxtree,counter + 2, repCounter)
+
+	elif l_input[counter - 1] == WHITESPACE_N:													#WHITESPACE
+		return parse(l_input,syntaxtree,counter + 1, repCounter)
+	
+
+	elif l_input[counter - 1] == REP_N:															#REPSTART
+		if l_input[counter] == WHITESPACE_N and isinstance(l_input[counter + 1],list):
+			if l_input[counter + 2] == WHITESPACE_N and l_input[counter + 3] == QUOTE_N:
+				syntaxtree.append([l_input[counter - 1], l_input[counter + 1][1]])
+				return parse(l_input, syntaxtree, counter + 5, repCounter + 1)
+				#TODO
+			else:
+				pass
+				#TODO
+				#OM DET ÄR EN UTAN CITATTECKEN
+	elif l_input[counter - 1] == QUOTE_N:														#REPSLUT
+		syntaxtree.append([l_input[counter - 1], 0])
+		return parse(l_input, syntaxtree, counter + 1, repCounter - 1)
+	elif l_input[counter - 1] == COLOR_N:														#COLOR
+		if l_input[counter] == WHITESPACE_N and l_input[counter + 1] == HASH_N:
+			if l_input[counter + 2] == PERIOD_N:
+				syntaxtree.append([l_input[counter - 1], l_input[counter + 1]])
+				return parse(l_input, syntaxtree, counter + 4, repCounter)
+			elif l_input[counter + 2] == WHITESPACE_N and l_input[counter + 3] == PERIOD_N:
+				syntaxtree.append([l_input[counter - 1], l_input[counter + 1]])
+				return parse(l_input, syntaxtree, counter + 5, repCounter)
 	return counter
 #TODO
 def error(pre_lex, n):
 	print "Syntaxfel på rad " + str(n)
 	return
-main()def error(pre_lex, n):
-	print "Syntaxfel på rad " + str(n)
-	return
+main()
